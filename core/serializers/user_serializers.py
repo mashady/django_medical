5a7f78211ababbdddd4a05a5f2ecf7cf507b4d82
Hi Heba <hebaelhalawany2000@gmail.com>
2025-06-07 22:04:46 +0300

    user seriailzer

--- core/serializers/user_serializers.py
+++ core/serializers/user_serializers.py
@@ -32,3 +32,4 @@ class RegisterUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
+    
