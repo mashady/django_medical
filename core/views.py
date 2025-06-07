eee6336bdb90abd5c880b3b8140403e9200bd1c2
Mostafa <mostafamokna78@gmail.com>
2025-06-07 23:42:23 +0300

    fix

--- core/views.py
+++ core/views.py
@@ -132,14 +132,16 @@ class PatientProfileViewSet(viewsets.ModelViewSet):
             return [IsPatientUser()]
         return [IsAuthenticated()]
 
+
+    @action(detail=False, methods=['post'], url_path='patients')
     def perform_create(self, serializer):
+        if PatientProfile.objects.filter(user=self.request.user).exists():
+            raise ValidationError("You already have a profile.")
         serializer.save(user=self.request.user)
+
 
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
-
-    def partial_update(self, request, *args, **kwargs):
-        return super().partial_update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
